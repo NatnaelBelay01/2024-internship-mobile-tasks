b4e9eef346405490f6ff9889afdf78526db82b6e
Xrash <natnaelbelayteklemariam@gmail.com>
2024-08-18 22:21:19 +0300

    Implementing remote data source with tests

--- mobile/ecommerce_clean_arc/ecommerce/lib/features/product/data/datasources/product_remote_data_source.dart
+++ mobile/ecommerce_clean_arc/ecommerce/lib/features/product/data/datasources/product_remote_data_source.dart
@@ -1,8 +1,66 @@
-import 'package:ecommerce/features/product/data/models/product_model.dart';
+import 'dart:convert';
 
-abstract class ProductRemoteDataSource{
+import 'package:ecommerce/core/error/exception.dart';
+import 'package:ecommerce/features/product/data/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+abstract class ProductRemoteDataSource {
   Future<ProductModel> createProduct(ProductModel product);
   Future<ProductModel> updateProduct(ProductModel product);
   Future<void> deleteProduct(String id);
   Future<ProductModel> getProuduct(String id);
+}
+
+class ProductRemoteDataSourceImp implements ProductRemoteDataSource {
+  ProductModel dummymodel = const ProductModel(
+      id: 'a', name: 'b', description: 'c', imageurl: 'd', price: 4);
+
+  final http.Client client;
+  ProductRemoteDataSourceImp({required this.client});
+
+  @override
+  Future<ProductModel> createProduct(ProductModel product) async {
+    final result = await client.post(
+        Uri.parse(
+            'https://g5-flutter-learning-path-be.onrender.com/${product.id}'),
+        body: product,
+        headers: {'Content-Type': 'application/json'});
+		if (result.statusCode == 201){
+    	return Future.value(ProductModel.fromJson(json.decode(result.body)));
+		} else{
+			throw(ServerException());
+		}
+  }
+
+  @override
+  Future<ProductModel> updateProduct(ProductModel product) async {
+		final result = await client.put(Uri.parse('https://g5-flutter-learning-path-be.onrender.com/${product.id}'), headers: {'Content-Type': 'application/json'}, body: product);
+		if(result.statusCode == 204){
+			return Future.value(product);
+		} else{
+			throw(ServerException());
+		}
+  }
+
+  @override
+  Future<void> deleteProduct(String id) async {
+		final result = await client.delete(Uri.parse('https://g5-flutter-learning-path-be.onrender.com/$id'));
+		if(result.statusCode == 204){
+			return Future.value(null);
+		} else{
+			throw(ServerException());
+		}
+  }
+
+  @override
+  Future<ProductModel> getProuduct(String id) async {
+    final result = await client.get(
+        Uri.parse('https://g5-flutter-learning-path-be.onrender.com/$id'),
+        headers: {'Content-Type': 'application/json'});
+    if (result.statusCode == 200) {
+      return Future.value(ProductModel.fromJson(json.decode(result.body)));
+    } else {
+      throw ServerException();
+    }
+  }
 }
---
+++ mobile/ecommerce_clean_arc/ecommerce/test/data/datasource/remote_data_source_test.dart
@@ -0,0 +1,83 @@
+import 'package:ecommerce/core/error/exception.dart';
+import 'package:ecommerce/features/product/data/models/product_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:ecommerce/features/product/data/datasources/product_remote_data_source.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import '../../fixtures/fixture.dart';
+import '../../helpers/repo_mocks/http/test_helper.mocks.dart';
+
+void main() {
+  late MockClient mockclient;
+  late ProductRemoteDataSourceImp remotedataImp;
+  late ProductModel productmodel;
+
+  setUp(() {
+    mockclient = MockClient();
+    remotedataImp = ProductRemoteDataSourceImp(client: mockclient);
+    productmodel = const ProductModel(
+      id: "01",
+      name: "shoe",
+      description: "a shoe",
+      imageurl: "img/b.jpg",
+      price: 34,
+    );
+  });
+
+  group('test for getproduct', () {
+    test('Should perform a GET request on the url', () async {
+      when(mockclient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('product.json'), 200));
+      remotedataImp.getProuduct('01');
+      verify(
+        mockclient.get(
+            Uri.parse('https://g5-flutter-learning-path-be.onrender.com/01'),
+            headers: {'Content-Type': 'application/json'}),
+      );
+    });
+
+    test('Should return ProducModel when the repose is successfull', () async {
+      when(mockclient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('product.json'), 200));
+      final result = await remotedataImp.getProuduct('01');
+      expect(result, productmodel);
+    });
+
+    test('should throw a serverException', () async {
+      when(mockclient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response('something went wrong', 404));
+      final call = remotedataImp.getProuduct;
+      expect(() => call('01'), throwsA(const TypeMatcher<ServerException>()));
+    });
+  });
+
+  group('createProduct', () {
+    test('Should perform a post request', () async {
+      when(mockclient.post(any,
+              body: anyNamed('body'), headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('product.json'), 201));
+      await remotedataImp.createProduct(productmodel);
+      verify(mockclient.post(
+          Uri.parse(
+              'https://g5-flutter-learning-path-be.onrender.com/${productmodel.id}'),
+          body: productmodel,
+          headers: {'Content-Type': 'application/json'}));
+    });
+
+    test('Should return a product model', () async {
+      when(mockclient.post(any,
+              body: anyNamed('body'), headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('product.json'), 201));
+      final result = await remotedataImp.createProduct(productmodel);
+      expect(result, productmodel);
+    });
+
+    test('should throw a serverException', () async {
+      when(mockclient.post(any,
+              body: anyNamed('body'), headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response('something went wrong', 404));
+			final call = remotedataImp.createProduct;
+			expect(() => call(productmodel), throwsA(const TypeMatcher<ServerException>()));
+    });
+  });
+}
